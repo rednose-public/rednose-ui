05edde409361fa839203aa04bfb7bfe391912708
Marc Bontje <marc@rednose.nl>
2012-12-13 21:54:23 +0100

    Added bootstrap script

---
+++ src/common/node/batch.js
@@ -0,0 +1,16 @@
+th = require('path');
+
+var base = path.join(__dirname, '../../');
+
+var json = require('./parse').parse(function(line) {
+    return (line.indexOf('coverage') === -1);
+});
+var paths = require('./parse').paths(json);
+
+var out = [];
+
+paths.forEach(function(p, i) {
+    out.push(path.join(base, p));
+});
+
+module.exports = out;
