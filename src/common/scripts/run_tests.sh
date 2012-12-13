d27576544474564911110b91996ced51e6c33c3b
Marc Bontje <marc@rednose.nl>
2012-12-13 22:26:35 +0100

    Added jUnit report

--- src/common/scripts/run_tests.sh
+++ src/common/scripts/run_tests.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 sh src/common/scripts/bootstrap.sh
-cd web/components/yui3-libbit && grover -t 180 -c 5  -i src/common/node/batch.js
+rm -r reports
+mkdir reports
+cd web/components/yui3-libbit && grover -t 180 -c 5  -i src/common/node/batch.js -o ../../../reports/junit.xml --junit
