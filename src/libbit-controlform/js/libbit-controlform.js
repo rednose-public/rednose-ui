3de61f28568cd30c497b95f9d82e4b3c45a3e1ef
Sven Hagemann <sven@rednose.nl>
2012-12-17 14:58:06 +0100

    Control forms

--- src/libbit-controlform/js/libbit-controlform.js
+++ src/libbit-controlform/js/libbit-controlform.js
@@ -1,9 +1,13 @@
 var ControlForm;
 
 ControlForm = Y.Base.create('controlForm', Y.Base, [], {
+    model: null,
+
     render: function(formsModel)
     {
         var self = this;
+
+        self.model = formsModel;
 
         formsModel.each(function(formItem) {
             self.renderForm(formItem);
@@ -18,14 +22,14 @@ ControlForm = Y.Base.create('controlForm', Y.Base, [], {
 
         var formElement = Y.Node.create('<fieldset>');
         var legend = Y.Node.create('<legend>');
-        var list = Y.Node.create('<ol>');
 
         legend.set('innerHTML', form.get('caption'));
 
         formElement.append(legend);
-        formElement.append(list);
 
         Y.Array.each(fieldGroups, function(group) {
+            var list = Y.Node.create('<ol>');
+
             Y.Array.each(group['fieldGroupItems'], function(control) {
                 var label = Y.Node.create('<label>');
                 var controlContainer = Y.Node.create('<li>');
@@ -40,6 +44,8 @@ ControlForm = Y.Base.create('controlForm', Y.Base, [], {
 
                 list.append(controlContainer);
             });
+
+            formElement.append(list);
         });
 
         var directionClassName = container.getAttribute('class') + '_' + formItem.get('direction');
@@ -49,7 +55,29 @@ ControlForm = Y.Base.create('controlForm', Y.Base, [], {
         } else {
             container.append(formElement);
         }
-    }
+    },
+
+    ddOver: function(e, referenceForm)
+    {
+        var formNode = Y.one('#' + referenceForm.get('id'));
+
+        if (e.type == 'drop:over') {
+            if (formNode.hasClass('ddOver') === false) {
+                formNode.addClass('ddOver');
+            }
+        } else {
+            formNode.removeClass('ddOver');
+        }
+    },
+
+    ddDrop: function(e, referenceForm)
+    {
+        var formNode = Y.one('#' + referenceForm.get('id'));
+
+        console.warn(e);
+
+        formNode.removeClass('ddOver');
+    },
 }, {
     ATTRS: {
         formContainer: { value: '' }
