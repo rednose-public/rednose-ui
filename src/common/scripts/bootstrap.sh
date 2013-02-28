4bd83745cc7f292b160e56f2cb750a5344a5402f
Marc Bontje <marc@rednose.nl>
2013-02-28 17:49:59 +0100

    Cleanup

--- src/common/scripts/bootstrap.sh
+++ src/common/scripts/bootstrap.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 rm -r web/components/yui3-libbit
-mkdir web/components/yui3-libbit
+mkdir -p web/components/yui3-libbit
 cp -r build web/components/yui3-libbit/build
 cp -r src web/components/yui3-libbit/src
 
-git submodule init
-git submodule update
+git clone git@gitorious.rednose.nl:yui/yui3.git web/components/yui3
+git clone git@gitorious.rednose.nl:yui/yui3-gallery.git web/components/yui3-gallery
