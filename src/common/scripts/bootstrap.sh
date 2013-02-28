fdb30fcdc8420dc26be3e1e2deaa4698e0d947e9
Marc Bontje <marc@rednose.nl>
2013-02-28 17:53:23 +0100

    Cleanup

--- src/common/scripts/bootstrap.sh
+++ src/common/scripts/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -r web/components/yui3-libbit
+rm -r web
 mkdir -p web/components/yui3-libbit
 cp -r build web/components/yui3-libbit/build
 cp -r src web/components/yui3-libbit/src
