7dd5d9813295b92f8fac12c98b38de95cab37550
Marc Bontje <marc@rednose.nl>
2013-09-03 17:44:51 +0200

    Updated scripts

--- src/common/scripts/bootstrap.sh
+++ src/common/scripts/bootstrap.sh
@@ -5,5 +5,5 @@ mkdir -p web/components/rednose-ui
 cp -r build web/components/rednose-ui/build
 cp -r src web/components/rednose-ui/src
 
-git clone git@gitorious.rednose.nl:yui/yui3.git web/components/yui3
-git clone git@gitorious.rednose.nl:yui/yui3-gallery.git web/components/yui3-gallery
+git clone git@gitlab.rednose.nl:rednose/yui3.git web/components/yui3
+git clone git@gitlab.rednose.nl:rednose/yui3-gallery.git web/components/yui3-gallery
--- src/common/scripts/run_tests.sh
+++ src/common/scripts/run_tests.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 sh src/common/scripts/bootstrap.sh
-rm -rf reports
-mkdir reports
-#cd web/components/rednose-ui && grover -t 180 -c 5  -i src/common/node/batch.js -o ../../../reports/junit.xml --junit
-cd web/components/rednose-ui && yogi test --istanbul --coverage -o ../../../reports/junit.xml --junit
+cd web/components/rednose-ui && yogi test
