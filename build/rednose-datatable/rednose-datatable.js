a52680aa5647d9b7f1c0f43dbc8356f853a493e2
Marc Bontje <marc@rednose.nl>
2013-09-05 16:59:04 +0200

    Added custom classnames

--- build/rednose-datatable/rednose-datatable.js
+++ build/rednose-datatable/rednose-datatable.js
@@ -2,7 +2,15 @@ YUI.add('rednose-datatable', function (Y, NAME) {
 
 var DataTable;
 
-DataTable = Y.Base.create('dataTable', Y.DataTable.Base, [ Y.DataTable.Scrollable, Y.DataTable.Sortable ]);
+DataTable = Y.Base.create('dataTable', Y.DataTable.Base, [ Y.DataTable.Scrollable, Y.DataTable.Sortable ], {
+	getClassName: function(name) {
+		if (name === 'table') {
+			return 'table';
+		}
+
+		return 'rednose-ui-' + name;
+	}
+});
 
 // -- Namespace ----------------------------------------------------------------
 Y.namespace('Rednose').DataTable = DataTable;
--- src/rednose-datatable/js/rednose-datatable.js
+++ src/rednose-datatable/js/rednose-datatable.js
@@ -1,6 +1,14 @@
 var DataTable;
 
-DataTable = Y.Base.create('dataTable', Y.DataTable.Base, [ Y.DataTable.Scrollable, Y.DataTable.Sortable ]);
+DataTable = Y.Base.create('dataTable', Y.DataTable.Base, [ Y.DataTable.Scrollable, Y.DataTable.Sortable ], {
+	getClassName: function(name) {
+		if (name === 'table') {
+			return 'table';
+		}
+
+		return 'rednose-ui-' + name;
+	}
+});
 
 // -- Namespace ----------------------------------------------------------------
 Y.namespace('Rednose').DataTable = DataTable;
