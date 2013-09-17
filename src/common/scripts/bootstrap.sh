6121d2120a5ebf8e052f2107aca61de8b86c04a9
Marc Bontje <marc@rednose.nl>
2013-09-17 19:05:58 +0200

    Cleanup

--- src/common/scripts/bootstrap.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf web
-mkdir -p web/components/rednose-ui
-cp -r build web/components/rednose-ui/build
-cp -r src web/components/rednose-ui/src
-
-git clone git@gitlab.rednose.nl:rednose/yui3.git web/components/yui3
-git clone git@gitlab.rednose.nl:rednose/yui3-gallery.git web/components/yui3-gallery
--- src/common/scripts/run_tests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sh src/common/scripts/bootstrap.sh
-cd web/components/rednose-ui && yogi test
