3309acabc2fce9a4c6bbd08f5045e3ee4c572b5d
Marc Bontje <marc@rednose.nl>
2014-06-18 13:19:27 +0200

    GridView fixes

--- build/rednose-grid/rednose-grid-debug.js
+++ build/rednose-grid/rednose-grid-debug.js
@@ -7,7 +7,6 @@ var Grid,
 
 GridView = Y.Base.create('gridView', Y.View, [], {
 
-    // Compile our template using Handlebars.
     template: Y.Handlebars.compile(
         '<div class="model-grid-container" title="{{ name }}" data-yui3-record="{{ clientId }}">' +
             '<div class="model-grid-icon-container">' +
@@ -33,8 +32,6 @@ GridView = Y.Base.create('gridView', Y.View, [], {
         model.after('change', this.render, this);
     },
 
-    // Render this view in our <li> container, and fill it with the
-    // data in our Model.
     render: function () {
         var container = this.get('container'),
             model     = this.get('model'),
@@ -72,14 +69,11 @@ GridView = Y.Base.create('gridView', Y.View, [], {
         return this;
     },
 
-    // Turn on editing mode for the Template name by exposing the input field.
     edit: function () {
         this.get('footerNode').addClass('editing');
         this.get('inputNode').focus();
     },
 
-    // Get the value from our input field while hiding it, and
-    // save it to our Model (name attribute) when focus is lost from the field.
     close: function () {
         var value       = this.get('inputNode').get('value'),
             self        = this,
@@ -101,7 +95,6 @@ GridView = Y.Base.create('gridView', Y.View, [], {
         }
     },
 
-    // Also allow updating the Template's name through the enter key.
     enterUpdate: function (e) {
         var ENTER_KEY = 13;
 
--- src/rednose-grid/js/grid.js
+++ src/rednose-grid/js/grid.js
@@ -5,7 +5,6 @@ var Grid,
 
 GridView = Y.Base.create('gridView', Y.View, [], {
 
-    // Compile our template using Handlebars.
     template: Y.Handlebars.compile(
         '<div class="model-grid-container" title="{{ name }}" data-yui3-record="{{ clientId }}">' +
             '<div class="model-grid-icon-container">' +
@@ -31,8 +30,6 @@ GridView = Y.Base.create('gridView', Y.View, [], {
         model.after('change', this.render, this);
     },
 
-    // Render this view in our <li> container, and fill it with the
-    // data in our Model.
     render: function () {
         var container = this.get('container'),
             model     = this.get('model'),
@@ -70,14 +67,11 @@ GridView = Y.Base.create('gridView', Y.View, [], {
         return this;
     },
 
-    // Turn on editing mode for the Template name by exposing the input field.
     edit: function () {
         this.get('footerNode').addClass('editing');
         this.get('inputNode').focus();
     },
 
-    // Get the value from our input field while hiding it, and
-    // save it to our Model (name attribute) when focus is lost from the field.
     close: function () {
         var value       = this.get('inputNode').get('value'),
             self        = this,
@@ -99,7 +93,6 @@ GridView = Y.Base.create('gridView', Y.View, [], {
         }
     },
 
-    // Also allow updating the Template's name through the enter key.
     enterUpdate: function (e) {
         var ENTER_KEY = 13;
 
