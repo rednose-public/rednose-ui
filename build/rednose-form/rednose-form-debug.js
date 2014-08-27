8e89cdb74e6fa8f709aa9f040fc684ab9a440a10
Marc Bontje <marc@rednose.nl>
2014-08-27 12:17:49 +0200

    Buildfix

--- build/rednose-form/rednose-form-debug.js
+++ build/rednose-form/rednose-form-debug.js
@@ -1418,7 +1418,6 @@ FormXML.prototype = {
     },
 
     getXpathValue: function (query) {
-            Y.log('test', 'info');
         var result = this.xml.evaluate(query, this.xml, null, XPathResult.ANY_TYPE, null);
 
         if (result.resultType === 2) {
